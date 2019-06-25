78b6b1624581590a85396e4513699a4ce5ecc633
Weikai Wang <WeWang@suse.com>
2019-06-25 21:53:11 +0800

    update some front display

--- public/datas/nav.js
+++ public/datas/nav.js
@@ -1,44 +1,19 @@
 var navs = [{
-	"title": "基本元素",
+	"title": "dashboard",
 	"icon": "fa-cubes",
 	"spread": true,
 	"children": [{
-		"title": "按钮",
-		"icon": "&#xe641;",
-		"href": "button.html"
-	}, {
-		"title": "表单",
+		"title": "All Memo Entries",
 		"icon": "&#xe63c;",
-		"href": "form.html"
-	}, {
-		"title": "表格",
-		"icon": "&#xe63c;",
-		"href": "table.html"
-	}, {
-		"title": "导航",
-		"icon": "&#xe609;",
-		"href": "nav.html"
-	}, {
-		"title": "Tab选项卡",
-		"icon": "&#xe62a;",
-		"href": "tab.html"
-	}, {
-		"title": "辅助性元素",
-		"icon": "&#xe60c;",
-		"href": "auxiliar.html"
+		"href": "/records"
 	}]
 }, {
 	"title": "Setting",
 	"icon": "fa-cogs",
-	"spread": false,
-	/*
+	"spread": true,
 	"children": [{
 		"title": "Datatable",
-		"icon": "fa-table",
-		"href": "begtable.html"
-	}, {
-		"title": "Navbar组件",
-		"icon": "fa-navicon",
-		"href": "navbar.html"
-	}]*/
+		"icon": "fa-cogs",
+		"href": "/setting"
+	}]
 }];
