46178c155ad824ae9e660d8ae58414fd3a4939ac
Weikai Wang <WeWang@suse.com>
2019-06-28 00:07:35 +0800

    support multi-line in suitable size

--- api/paper_hb_querry.js
+++ api/paper_hb_querry.js
@@ -10,29 +10,50 @@ var floydSteinberg = require('floyd-steinberg');
 const svg2img = require('svg2img');
 const fs = require('fs')
 var EventProxy = require('eventproxy');
-const options = require('../config/options')
+const options = require('../config/options.js')
+const util = require('../util/util.js')
+const get_show_len = require('../util/get_show_len.js')
+
 
 router.post('/', (req, res) => {
     // console.log(req.body);
     // Data.now().getTime().toString()
+    var width = 400;
+    var height = 25;
     var ep = new EventProxy();
-    ep.after("gotline", 6, function (lines) {
-        handle_bin_img(Buffer.concat(lines), req, res);
+    ep.after("gotline", 12, function (lines) {
+      handle_bin_img(Buffer.concat(lines), req, res);
     });
     User.findOne({ paper_id: req.body.paper_id }).then(user => {
         if(user){
             MemoRecord.find({ email: user.email }, null, {sort:{date: -1}}).then(records => {
             //console.log(records)
             var img_data;
-            for(let i=0; i<6; i++){
-              record = records[i].record
+            for(let i=0; i<12; i++){
+
+              //record = records[i].record
+
+              if(i >= records.length)
+                record = " "
+              else
+                record = records[i].record
+
+
+              if(get_show_len(record) < 30){
+                let tmp_len = 30 - get_show_len(record);
+                while(tmp_len--)
+                  record += " "
+              }
               const svg = textToSVG.getSVG(record, options);
-              svg2img(svg, {'width':400, 'height':50} , function(error, buffer) {
+              const metrics = textToSVG.getMetrics(record, options)
+                svg2img(svg, {'width':width, 'height':height} , function(error, buffer) {
                   //returns a Buffer
                   let img_name = 'part_img_'+i+'.png';
+
                   fs.writeFileSync(img_name, buffer);
+
                   fs.createReadStream(img_name).pipe(new PNG()).on('parsed', function() {
-                      ep.emit('gotline', floydSteinberg(this).data)
+                    ep.emit('gotline', floydSteinberg(this).data);
                   })
               });
             }
--- config/font_file.js
+++ config/font_file.js
@@ -1,4 +1,5 @@
 const TextToSVG = require('text-to-svg');
-const textToSVG = TextToSVG.loadSync(require('path').dirname(__filename)+"/../config/Mofita.otf");
+const textToSVG = TextToSVG.loadSync(require('path').dirname(__filename)+"/../config/C301.ttf");
+//const textToSVG = TextToSVG.loadSync();
 
 module.exports = textToSVG
