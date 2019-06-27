385a0d9c699f8127b3dbda937692c3bb246df177
Weikai Wang <WeWang@suse.com>
2019-06-27 09:01:21 +0800

    add partial fuction of paper_hb_handler and update User.date when add new item(modify should also)

--- api/paper_hb_handler.js
+++ api/paper_hb_handler.js
@@ -5,7 +5,23 @@ const User = require('../models/User');
 
 router.post('/', (req, res) => {
     console.log(req.body);
-    res.end("UPDATE")
+    // Data.now().toString()
+    User.findOne({ paper_id: paper_id }).then(user => {
+        if(user){
+            if(req.body.update_time === user.date.toString())
+              res.end('N')
+            // in js, can compaer string with int number
+            if(req.body.update_time < user.date)
+              res.end('Y')
+            if(req.body.update_time > user.date)
+              res.end('W')
+        }
+        else{
+          // N represent no update
+          // C represent can't find the device
+          res.end("C")
+        }
+    });
 });
 
 module.exports = router;
--- routes/addrecord.js
+++ routes/addrecord.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated, initAuthenticated } = require('../config/auth');
 const MemoRecord = require('../models/MemoRecord');
+const User = require('../models/User');
+
+function touch_user_date(email){
+    User.findOne({ email: email }).then(user => {
+      console.log(user.date);
+      user.date = Date.now()
+      user.save()
+    });
+}
 
 router.get('/', ensureAuthenticated, (req, res) =>
   res.render('addrecord', {
@@ -40,10 +49,12 @@ router.post('/', ensureAuthenticated, (req, res) => {
     newRecord
       .save()
       .then(user => {
+        touch_user_date(req.user.email)
         req.flash(
           'success_msg',
           'You are now registered and can log in'
         );
+
         res.redirect('/records');
       })
       .catch(err => console.log(err));
@@ -58,6 +69,7 @@ router.post('/simple', ensureAuthenticated, (req, res) => {
     newRecord
       .save()
       .then(user => {
+        touch_user_date(req.user.email)
         req.flash(
           'success_msg',
           'You are now registered and can log in'
